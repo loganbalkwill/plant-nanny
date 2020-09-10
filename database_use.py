5ad4f47c73c73b72f02f17216719af408a1fd38f
loganbalkwill <lgb0020@gmail.com>
2020-09-10 16:54:15 -0600

    converted PlantDB class to variable and independant methods

--- database_use.py
+++ database_use.py
@@ -2,7 +2,7 @@
 #Created on 8-30-2020
 
 import mysql.connector
-import settings
+import settings as s
 
 
 """ TODO
@@ -10,48 +10,36 @@ import settings
         -
 """
 
-class Plant_Database:
-    """Connector to plant database. Also provides writing functionality
-    """
+try:
+    plant_db=mysql.connector.connect(host=s.host,
+                                     user=s.username,
+                                     password=s.password,
+                                     database=s.database_name)
     
-    def __init__(self):
-        
-        #Import database properties from settings.py
-        self.host=settings.host
-        self.username=settings.username
-        self.password=settings.password
-        self.db_name=settings.database_name
+    print("connection to database '%s' was successfull" % s.database_name)
+    
+except:
+    print("FAILED to connect to database '%s'" % s.database_name) 
 
-    def connect(self):
-        #Attempt to make a connection to the database
-        try:
-            mysql.connector.connect(host=self.host,user=self.username,password=self.password,database=self.db_name)
-            print("connection to database '%s' was successfull" % self.db_name)
-        except:
-            print("FAILED to connect to database '%s'" % self.db_name) 
 
-    def write(self, table, write_info):
-        #Write data to the database table
+def write_to_db(db, table, write_info):
+    #Write data to the database table
+    
+    #build SQL command string
+    sql=build_SQL_insert(table)
+    
+    #insert into database
+    try:
+        cursor=db.cursor()
+        cursor.execute(sql, write_info)
         
-        #build SQL command string
-        sql=build_SQL_insert(table)
+        db.commit()
         
-        #insert into database
-        try:
-            cursor=self.connect.cursor()
-            cursor.execute(sql, val)
-            
-            self.commit()
-            
-            print(cursor.rowcount, "record inserted")
-        
-        except:
-            print("Failed to write to database")
+        print(cursor.rowcount, "record inserted")
+    
+    except:
+        print("Failed to write to database")
 
-        #mydb.commit()
-
-        #print(mycursor.rowcount, "record inserted.")
-        #print(sql)
 
 
 def build_SQL_insert(table_name):
@@ -65,6 +53,4 @@ def build_SQL_insert(table_name):
 
 if __name__=="__main__":
     print("Attempting DB Connection")
-    x=Plant_Database()
-    x.connect()
-    x.write(table='soilsensor',write_info=['2020-08-31','testPlant',20,390])
+    write_to_db(db=plant_db,table='soilsensor',write_info=['2020-08-31','testPlant',20,390])
