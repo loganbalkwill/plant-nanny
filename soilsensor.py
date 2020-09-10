32d769e49a3457cdc9d4baa1426231fb1ee27b13
loganbalkwill <lgb0020@gmail.com>
2020-09-10 17:16:47 -0600

    added standalone & module functions to soilsensor.py

--- soilsensor.py
+++ soilsensor.py
@@ -4,17 +4,29 @@ from board import SCL, SDA
 import busio
 
 from adafruit_seesaw.seesaw import Seesaw
+import settings
 
 i2c_bus = busio.I2C(SCL, SDA)
 
-ss = Seesaw(i2c_bus, addr=0x36)
+ss = Seesaw(i2c_bus, addr=settings.addr_sensor_soil)
 
-while True:
-    # read moisture level through capacitive touch pad
-    touch = ss.moisture_read()
 
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
+def get_moisure():
+    #retrieve latest moisture value
+    return ss.moisture_read()
 
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
-    time.sleep(1)
+def get_temp():
+    #retrieve latest temperature value (Degrees Celcius)
+    return ss.get_temp()
+
+
+if __name__=='__main__':
+    while True:
+        # read moisture level through capacitive touch pad
+        touch = ss.moisture_read()
+
+        # read temperature from the temperature sensor
+        temp = ss.get_temp()
+
+        print("temp: " + str(temp) + "  moisture: " + str(touch))
+        time.sleep(1)
