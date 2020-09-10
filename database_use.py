3a39334321ce948da4185278cc6b956cd2651666
loganbalkwill <lgb0020@gmail.com>
2020-09-10 16:44:12 -0600

    added writing functionality to database class

--- database_use.py
+++ database_use.py
@@ -4,6 +4,12 @@
 import mysql.connector
 import settings
 
+
+""" TODO
+        -Logging database actions
+        -
+"""
+
 class Plant_Database:
     """Connector to plant database. Also provides writing functionality
     """
@@ -24,8 +30,41 @@ class Plant_Database:
         except:
             print("FAILED to connect to database '%s'" % self.db_name) 
 
+    def write(self, table, write_info):
+        #Write data to the database table
+        
+        #build SQL command string
+        sql=build_SQL_insert(table)
+        
+        #insert into database
+        try:
+            cursor=self.connect.cursor()
+            cursor.execute(sql, val)
+            
+            self.commit()
+            
+            print(cursor.rowcount, "record inserted")
+        
+        except:
+            print("Failed to write to database")
+
+        #mydb.commit()
+
+        #print(mycursor.rowcount, "record inserted.")
+        #print(sql)
+
+
+def build_SQL_insert(table_name):
+    #returns sql string of command
+    
+    if table_name=='soilsensor':
+        return "INSERT INTO soilsensor (DateTime, plant_id, SoilTemp_DegC, SoilMoisture_val) VALUES (%s, %s, %s, %s)"
+    else:
+        return ''
+
 
 if __name__=="__main__":
     print("Attempting DB Connection")
     x=Plant_Database()
     x.connect()
+    x.write(table='soilsensor',write_info=['2020-08-31','testPlant',20,390])
