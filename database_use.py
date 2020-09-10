7d760c10ab45d867ea8f77023fd900ca8a41d993
loganbalkwill <lgb0020@gmail.com>
2020-09-10 15:15:45 -0600

    imported database properties from settings.py into database class within database_use.py

--- database_use.py
+++ database_use.py
@@ -2,6 +2,7 @@
 #Created on 8-30-2020
 
 import mysql.connector
+import settings
 
 db_driver=''
 db_server=''
@@ -14,5 +15,12 @@ class Plant_Database:
     
     def __init__(self):
         
+	#Import database properties from settings.py
+	self.host=settings.host
+	self.username=settings.username
+	self.password=settings.password
+	self.db_name=settings.database_name
+
+    def connect(self):
         #Attempt to make a connection to the database
-        return true
+        
