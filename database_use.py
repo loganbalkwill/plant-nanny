e453d9fda24ca7410d01e2b8ffca6db28af57300
loganbalkwill <lgb0020@gmail.com>
2020-09-10 14:30:54 -0600

    Imported database_use.py from local folder

---
+++ database_use.py
@@ -0,0 +1,18 @@
+#Created by Logan Balkwill
+#Created on 8-30-2020
+
+import mysql.connector
+
+db_driver=''
+db_server=''
+db_name='plantnanny'
+
+
+class Plant_Database:
+    """Connector to plant database. Also provides writing functionality
+    """
+    
+    def __init__(self):
+        
+        #Attempt to make a connection to the database
+        return true
