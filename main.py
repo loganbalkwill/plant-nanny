008a440290ca043ddf469db376d9bbc9dafaf09c
loganbalkwill <lgb0020@gmail.com>
2020-09-10 14:46:40 -0600

    Added main.py file with basic information

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""main.py:    
+               -hosts main project code
+               -hosts list and notes for unfinished project sections
+"""
+
+__author__    = "Logan Balkwill"
+__date__      = "September 10, 2020"
+__version__   = "1.0.1"
+__maintainer__= "Logan Balkwill"
+__email__=    = "lgb0020@gmail.com"
+
+
