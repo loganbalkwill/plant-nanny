fd39e74ded380c02d3e3d42b9fbd9e5965848d87
loganbalkwill <lgb0020@gmail.com>
2020-09-14 18:19:22 -0600

    modified loop frequency calculation to comply with new input style

--- supporting_functions.py
+++ supporting_functions.py
@@ -1,24 +1,25 @@
 import math
+import settings
 
 def get_loop_frequency(numlist):
     #returns the frequency of looping in the main procedure
     
     #default returns 1 minute
-    GCD=1
+    GCD=settings.read_frequency_mins
     reclist=[]
     #if any of the numbers provided are less than 1 minute (non-negative),
     #a greatest common divisor (GCD) is calculated
     
-    for record in numlist:
-        for num in record:
-            if num<0: 
-                print("ERROR: negative number provided for loop frequency; please correct")
-                break
-            elif num>1 and num!=math.ceil(num):
-                print("WARNING: partial minute provided for loop frequency (%s mins); number will be rounded to %s mins for calculation" % (num, math.floor(num)))
-                
-                num=math.floor(num)
-            reclist.append(num)
+    for num in numlist:
+        if num<0: 
+            print("ERROR: negative number provided for loop frequency; please correct")
+            #This shouldn't happen because of the database table settings
+            break
+        elif num>1 and num!=math.ceil(num):
+            print("WARNING: partial minute provided for loop frequency (%s mins); number will be rounded to %s mins for calculation" % (num, math.floor(num)))
+            
+            num=math.floor(num)
+        reclist.append(num)
     
     num_min=min(reclist)
     if num_min<1 and num_min>0:
