5e3dcda5ca76502befe20a0a8a9a93eb4042087e
loganbalkwill <lgb0020@gmail.com>
2020-09-16 17:57:48 -0600

    expanded database_use.py to include APDS9960 sensor functions

--- database_use.py
+++ database_use.py
@@ -44,8 +44,10 @@ def build_SQL_insert(table_name):
     
     if table_name=='soilsensor_trans':
         return "INSERT INTO soilsensor_trans (DateTime, plant_id, SoilTemp_DegC, SoilMoisture_val) VALUES (%s, %s, %s, %s)"
-    if table_name=='gassensor_trans':
+    elif table_name=='gassensor_trans':
         return "INSERT INTO `gassensor_trans` (`record_id`, `DateTime`, `plant_id`, `eCO2_ppm`, `TVOC_ppb`) VALUES (NULL, %s, %s, %s, %s)"
+    elif table_name=='
+        return "INSERT INTO `lightsensor_trans` (`record_id`, `DateTime`, `plant_id`, `colour_red`, `colour_green`, `colour_blue`, `colour_clear`) VALUES (NULL, NOW(), %s, %s, %s, %s, %s)"
     else:
         return ''
 
