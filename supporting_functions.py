7e64531eea5a375ac3e7e36947ec1916f17958bb
loganbalkwill <lgb0020@gmail.com>
2020-09-19 20:11:32 -0600

    improved find_i2c_devices function

--- supporting_functions.py
+++ supporting_functions.py
@@ -151,12 +151,35 @@ def perform_action(action):
 def find_i2c_devices():
     #Called when main.py is initialized, then regularly
     #used to detect available I2C devices
-
+    device_list=[]
+    
     p = subprocess.Popen(['i2cdetect', '-y','1'],stdout=subprocess.PIPE,) 
     #cmdout = str(p.communicate())
-
-    for i in range(0,9):
-      line = str(p.stdout.readline())
-
-    print(line)
     
+    for i in range(0,9):
+        line = str(p.stdout.readline())
+        #print(line)
+        
+        #Parse line for addresses
+        if i==0:
+            pass #Header row; no useful information available
+        else:
+            line_list=line.split()
+            for addr in line_list:
+                try:
+                    addr=int(addr)
+                    device_list.append(addr)
+                except:
+                    pass
+    
+    print(device_list)
+
+
+# 00:          -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 30: -- -- -- -- -- -- 36 -- -- 39 -- -- -- -- -- -- 
+# 40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+# 70: -- -- -- -- -- -- -- 77      
