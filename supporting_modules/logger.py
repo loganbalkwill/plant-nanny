06125a06de5544a8344cc4f34b2307a92039125f
loganbalkwill <lgb0020@gmail.com>
2020-09-22 15:25:24 -0600

    renamed logging.py to logger.py

---
+++ supporting_modules/logger.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import sys
+sys.path.insert(0, '..')
+
+import settings
+import supporting_modules.database_use as db
+
+def log_action(event,result, additional_info=''):
+    #break apart event into components
+    plant_id, plant_name, device_id, device_name, action_freq=event
+    
+    
+    #create writing information
+    if result=='success':
+        severity='i' #informational
+        message_prefix='Successfully performed device action(s): '
+    elif result=='failure':
+        severity='w' #warning
+        message_prefix='Failed to performe device action(s): '
+        
+    message_base= 'plant_id= %s, plant_name= %s, device_id= %s, device_name= %s' % (plant_id, plant_name, device_id, device_name)
+    
+    message=message_prefix + message_base
+    
+    if additional_info!='':
+        message=message + '; ' + additional_info
+            
+    log_info(severity,message)
+    
+    
+    
+def log_info(log_level,message):
+    
+    if log_level in settings.log_levels:
+        db.write_to_db(table='log_trans',
+                                 write_info=[datetime.now(), log_level, message])
