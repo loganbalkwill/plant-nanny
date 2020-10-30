b4e5b14f5b5b94f65827ce1092f0de0f08bc4220
loganbalkwill <lgb0020@gmail.com>
2020-10-29 19:26:55 -0600

    Created generic function for handling SELECT sql queries

--- supporting_modules/database_use.py
+++ supporting_modules/database_use.py
@@ -147,20 +147,18 @@ def retrieve_data(sql_query, db=plant_db):
     #generic function to run sql_query and return result object
     
     #Validate the query string
-    if sql_query.find('SELECT')=-1 AND sql_query.find('SELECT')=-1:
+    if (sql_query.find('SELECT')==-1) and (sql_query.find('select')==-1):
         #INVALID!! the provided string may be a 'UPDATE','DELETE', or other style string
         raise Exception('Error retrieving info from database... SQL string does not contain a SELECT statement!!')
-        break
     
     try:
-        mycursor=db.cursor()  
-        mycursor.execute(sql)
+        mycursor=db.cursor()
+        mycursor.execute(sql_query)
         query_results=mycursor.fetchall()
-        mycursor.close()
+        mycursor.close() 
     except:
         raise Exception('Unable to run SQL query. Verify the SQL string and database connection information')
-        break
-    
+   
     return query_results
 
 if __name__=="__main__":
