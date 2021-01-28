10e62850815468416fb0fb69f4a60692d305f1fc
loganbalkwill <lgb0020@gmail.com>
2021-01-27 22:11:39 -0600

    made begin_session more functional, lol

--- Plant-Nanny/supporting_modules/Peripherals/lightsensor.py
+++ Plant-Nanny/supporting_modules/Peripherals/lightsensor.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 #Light Sensor Service
+
+__status__ = "Production"
+
 import adafruit_apds9960.apds9960 #APDS9960 light sensor
 from board import SCL, SDA
 import busio
@@ -11,24 +14,26 @@ import sys
 #Define I2C interface
 i2c = busio.I2C(SCL, SDA, frequency=100000)
 
-def begin_session( Assigned_id, sleep_interval = 1, read_freq_mins=1, start_with_read=True ):
+def begin_session( Assigned_id='', sleep_interval = 1, read_freq_mins=1, start_with_read=True ):
     #CALLED FROM THE APPLICATION
     #initialize device
     try:
         apds = adafruit_apds9960.apds9960.APDS9960(i2c)
         apds.enable_color=True
-        send_health_info(Assigned_id,__file__,"Light Sensor Service (APDS9960) has started")
+        send_health_info(assigned_id=Assigned_id , service=__file__ , result="Success" , msg="Light Sensor Service (APDS9960) has started")
     except:
-        send_health_info(Assigned_id,__file__,)
-     
+        send_health_info(assigned_id=Assigned_id , service=__file__ , result="Failure" , msg="Light Sensor Service (APDS9960) could not be started")
+        exit
+    
+    #begin 
     while True:
-        if 
-        r,g,b,c=sensors.get_light_rgbc()
-        
-        database_use.write_to_db(table='lightsensor_trans',
+        r,g,b,c=apds.color_data
+        print("Red: %s; Green: %s; Blue: %s; Clear: %s"%(r,g,b,c))
+        time.sleep(sleep_interval)
+        """database_use.write_to_db(table='lightsensor_trans',
                                  write_info=[datetime.now(),
                                  Assigned_id,
-                                 r, g, b, c])
+                                 r, g, b, c])"""
 
 def testing_mode (sleep_interval = 1):
     try:
@@ -56,14 +61,24 @@ def exceptionHandler(exctype, val, tb):
     print('    Value: %s', val)
     print('    Traceback: %s', tb)
 
-sys.excepthook=exceptionHandler
+
+def send_health_info(assigned_id,service,result, msg):
+    #DUMMY FUNCTION
+    #TODO create actual function (in logger?)
+    print(msg)
+
 atexit.register(pre_exit)
 
 if __name__=='__main__':
-    #Assume it's being called in TESTING mode
-    try:
-        print("Beginning Sensor Test Mode")
-        testing_mode()
-    except:
-        #Error encountered during operation
-        print("ERROR")
+    
+    if __status__=='Testing':
+        try:
+            print("Beginning Light Sensor Service (Test Mode)")
+            testing_mode()
+        except:
+            #Error encountered during operation
+            print("ERROR")
+    elif __status__=='Production':
+        print("Beginning Light Sensor Service")
+        begin_session()
+        
