e3d004e0bcf3e20cd283399dc29a412443275ea6
loganbalkwill <lgb0020@gmail.com>
2021-02-04 22:22:41 -0600

    Added sql_select query strings (incomplete)

--- Plant-Nanny/supporting_modules/dictionaries.py
+++ Plant-Nanny/supporting_modules/dictionaries.py
@@ -13,12 +13,18 @@ sql_insert={
 }           
 
 #Retrieving information from database
+#TODO Complete SQL queries
 sql_select={
     'air_temp'          :   "SELECT CAST(a.DateTime as Time) as Timestamp, a.AirTemp_DegC FROM airsensor_trans a WHERE a.DateTime >= '%s' AND a.DateTime < '%s' AND a.room_id= '%s'",
     'air_humidity'      :   "SELECT CAST(a.DateTime as Time) as Timestamp, a.AirHumidity_percent FROM airsensor_trans a WHERE a.DateTime >= '%s' AND a.DateTime < '%s' AND a.room_id= '%s'",
     'sensors'           :   "SELECT * FROM sensors",
     'sensor_freq'       :   "SELECT read_frequency_min FROM sensors",
-    'device_assignments':   "SELECT DA.`id_room` AS Assigned_ID, 'Environment' AS Assigned_Type, E.`name_short` AS Assigned_Name, DA.`id_device`, D.`model` AS Device_Name, COALESCE(DA.`action_freq_mins`, D.`default_action_freq_mins`) AS Action_Frequency FROM `def_device_assignments` AS DA LEFT JOIN `def_devices` AS D ON DA.`id_device`=D.id LEFT JOIN `def_environments` AS E ON DA.id_room=E.id_room WHERE DA.`active`=1 AND E.`active`=1 AND D.`supported`=1 AND DA.id_room>0 UNION SELECT DA.`id_plant` AS Assigned_ID, 'Plant' AS Assigned_Type, P.`name` AS Assigned_Name, DA.`id_device`, D.`model` AS Device_Name, COALESCE(DA.`action_freq_mins`, D.`default_action_freq_mins`) AS Action_Frequency FROM `def_device_assignments` AS DA LEFT JOIN `def_devices` AS D ON DA.`id_device`=D.id LEFT JOIN `def_plants` AS P ON DA.id_plant=P.id WHERE DA.`active`=1 AND P.`active`=1 AND D.`supported`=1 AND DA.id_plant>0"
+    'device_assignments':   "SELECT DA.`id_room` AS Assigned_ID, 'Environment' AS Assigned_Type, E.`name_short` AS Assigned_Name, DA.`id_device`, D.`model` AS Device_Name, COALESCE(DA.`action_freq_mins`, D.`default_action_freq_mins`) AS Action_Frequency FROM `def_device_assignments` AS DA LEFT JOIN `def_devices` AS D ON DA.`id_device`=D.id LEFT JOIN `def_environments` AS E ON DA.id_room=E.id_room WHERE DA.`active`=1 AND E.`active`=1 AND D.`supported`=1 AND DA.id_room>0 UNION SELECT DA.`id_plant` AS Assigned_ID, 'Plant' AS Assigned_Type, P.`name` AS Assigned_Name, DA.`id_device`, D.`model` AS Device_Name, COALESCE(DA.`action_freq_mins`, D.`default_action_freq_mins`) AS Action_Frequency FROM `def_device_assignments` AS DA LEFT JOIN `def_devices` AS D ON DA.`id_device`=D.id LEFT JOIN `def_plants` AS P ON DA.id_plant=P.id WHERE DA.`active`=1 AND P.`active`=1 AND D.`supported`=1 AND DA.id_plant>0",
+    'room_name'         :   "",
+    'plant_name'        :   "",
+    'device_name'       :   "",
+    'action_frequecy'   :   "",
+    'script_name'       :   ""
 }
 
 """ Error Handling Dictionaries:    """
