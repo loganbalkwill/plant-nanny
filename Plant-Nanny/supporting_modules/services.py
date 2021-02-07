70f55bde3cd5fa98ce53d42058614aa8a21c302f
loganbalkwill <lgb0020@gmail.com>
2021-02-07 12:25:58 -0700

    Created new class for running services

---
+++ Plant-Nanny/supporting_modules/services.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" services.py
+    -used to handle all services 
+"""
+
+class service():
+    Status='stopped'
+
+    def __init__(self):
+        #initial setup
+        
+    def start():
+        #prepares to run service
+
+    def terminate():
+        #perform post-script stuff
+
+    def update_status():
+        #sends service status info to database
+
+    def run_service():
+        #
