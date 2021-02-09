45e43f4130f75849288885bfd673643a77e854a1
loganbalkwill <lgb0020@gmail.com>
2021-02-08 20:54:38 -0700

    wrote validate_assignmentcontext routine

--- Plant-Nanny/supporting_modules/services.py
+++ Plant-Nanny/supporting_modules/services.py
@@ -16,6 +16,9 @@ class service():
     def __init__(self, AssignmentContext, ScriptContext):
         #initial setup
         try:
+            #TODO validate information to make sure the service can run
+            validate_assignmentcontext(AssignmentContext)
+            validate_scriptcontext(ScriptContext)
             self.RoomID, self.RoomName, self.PlantID, self.PlantName, self.DeviceID, self.DeviceName = AssignmentContext
             self.ScriptName, self.ActionFrequency_mins, self.StartWithAction = ScriptContext
         except:
@@ -23,7 +26,6 @@ class service():
 
     def start(self):
         #prepare to run service
-            #TODO validate information to make sure the service can run
             #TODO condition/transform info for easier use in loop
                 #TODO convert action frequency from min to sec
         self.looplength=60*self.ActionFrequency_mins/self.LoopFrequency
@@ -74,6 +76,24 @@ class service():
                 #TODO send diagnostics
         
         #TODO service is no longer running; do post-service stuff
+
+def validate_assignmentcontext(AssignmentContext):
+    #Checks that the script data supplied to the service is usable
+    RoomID, RoomName, PlantID, PlantName, DeviceID, DeviceName = AssignmentContext
+    
+    #Variable Checks
+    assert isinstance(RoomID, int) or RoomID is None , "RoomID was not assigned properly"
+    assert isinstance(PlantID, int) or PlantID is None, "PlantID was not assigned properly"
+    assert isinstance(DeviceID, int) or DeviceID is None, "DeviceID was not assigned properly"
+
+    #Context Checks
+    assert ((RoomID is not None) or (PlantID is not None)), "RoomID and PlantID were not provided; at least one of these must be provided"
+    assert (RoomID )
+
+def validate_scriptcontext(ScriptContext):
+    #Checks that the script data supplied to the service is usable
+    ScriptName, ActionFrequency_mins, StartWithAction = ScriptContext
+
 
 if __name__ == "__main__":
     testService=service([1,"Test Room",None,None,2,"Test Plant"],["TestScript.py", 0.25 , True])
